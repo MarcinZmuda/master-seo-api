ac86b36611dd6fdf2c05e5ea57c9cd977c2a8100
Marcin Żmuda <132230638+MarcinZmuda@users.noreply.github.com>
2025-11-16 11:15:24 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Ten skrypt zapewnia, że Gunicorn jest uruchamiany w poprawnym kontekście
-
-# Przejdź do katalogu aplikacji (chociaż Dockerfile już to robi, to dodatkowe zabezpieczenie)
-cd /app
-
-# Uruchom serwer Gunicorn, wskazując mu bezpośrednio plik i aplikację
-exec gunicorn --bind 0.0.0.0:10000 master_api:app
