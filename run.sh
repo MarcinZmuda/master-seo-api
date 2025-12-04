86fef124e234b1a50e27a0a58dd6e8d10b0cc94e
Marcin Żmuda <132230638+MarcinZmuda@users.noreply.github.com>
2025-12-04 14:43:17 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ PORT=${PORT:-10000}
 
 echo "➡️ Starting Gunicorn on port ${PORT}..."
 
-# Uruchamiamy główną aplikację Flask: master_api:app
 exec gunicorn master_api:app \
     --bind 0.0.0.0:${PORT} \
     --workers 1 \
-    --timeout 120 \
+    --timeout 300 \
+    --preload \
     --log-level info
